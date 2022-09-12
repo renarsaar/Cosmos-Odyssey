91e7cc13f866a2928b10cd91cf6765936334f5a5
renarsaar <renar.saaremets@gmail.com>
2022-09-12 13:45:18 +0300

    deplaoyment changes

--- dist/index.js
+++ dist/index.js
@@ -11,9 +11,9 @@ const db_1 = require("./db");
 const reservations_1 = require("./routes/reservations");
 const priceLists_1 = require("./routes/priceLists");
 const job_1 = require("./job/job");
-const path_1 = __importDefault(require("path"));
 (0, dotenv_1.config)();
 const app = (0, express_1.default)();
+app.use(express_1.default.static('./build'));
 const allowedOrigins = ['http://localhost:3000', 'https://cosmosodysseyuptime.herokuapp.com/'];
 const allowedMethods = ['GET', 'POST'];
 const options = {
@@ -30,9 +30,6 @@ app.use(reservations_1.reservationsRouter);
 app.use(priceLists_1.priceListsRouter);
 (0, db_1.connectDb)();
 job_1.storePriceListsJob.invoke();
-app.get("*", (req, res) => {
-    res.sendFile(path_1.default.join(__dirname, "build", "index.html"));
-});
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
--- index.ts
+++ index.ts
@@ -8,11 +8,12 @@ import { connectDb } from './db';
 import { reservationsRouter } from './routes/reservations';
 import { priceListsRouter } from './routes/priceLists';
 import { storePriceListsJob } from './job/job';
-import path from 'path';
 
 config();
 
 const app: Express = express();
+
+app.use(express.static('./build'));
 
 const allowedOrigins = ['http://localhost:3000', 'https://cosmosodysseyuptime.herokuapp.com/'];
 const allowedMethods = ['GET', 'POST'];
@@ -22,6 +23,7 @@ const options: CorsOptions = {
 };
 
 app.use(cors(options));
+
 app.use(express.json());
 
 app.use('/TravelPrices', createProxyMiddleware({
@@ -35,10 +37,6 @@ app.use(priceListsRouter);
 connectDb();
 
 storePriceListsJob.invoke();
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
 
 const PORT: string | number = process.env.PORT || 8080;
 
